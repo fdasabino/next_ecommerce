db76898558b16b0f9379261b51582c752c5e6cc0
Francisco Sabino <fdasabino@gmail.com>
2023-06-08 23:15:05 +0200

    added db connection

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+let connection;
+// * connect to database
+const connectDB = async () => {
+  if (connection && mongoose.connection.readyState === 1) {
+    console.log("Already connected to database...");
+    return;
+  }
+
+  try {
+    connection = await mongoose.connect(process.env.NEXT_APP_MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("New database connection...");
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    throw new Error(error.message);
+  }
+};
+
+// * disconnect from database (only for production)
+const disconnectDB = async () => {
+  if (connection && mongoose.connection.readyState === 1) {
+    if (process.env.NODE_ENV === "production") {
+      await mongoose.disconnect();
+      connection.isConnected = false;
+    } else {
+      console.log("Not disconnected from database...");
+    }
+  }
+};
+
+const db = { connectDB, disconnectDB };
+export default db;
