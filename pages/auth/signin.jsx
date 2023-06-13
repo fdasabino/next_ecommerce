1957960a647620946c9383b1070668c756aca897
Francisco Sabino <fdasabino@gmail.com>
2023-06-13 20:40:28 +0200

    added provider sign in buttons to sign in page

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -2,10 +2,12 @@ import Button from "@/components/Layout/Button/Button";
 import FormInput from "@/components/Layout/Input/FormInput";
 import styles from "@/styles/pages/SignIn.module.scss";
 import { Form, Formik } from "formik";
+import { getProviders, signIn } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { BiLeftArrowAlt } from "react-icons/bi";
+import { FcGoogle } from "react-icons/fc";
 import * as Yup from "yup";
 
 const initialValues = {
@@ -13,7 +15,10 @@ const initialValues = {
   login_password: "",
 };
 
-const SignIn = () => {
+const SignIn = ({ providers }) => {
+  const MIN_PASSWORD_LENGTH = 6;
+  const MAX_PASSWORD_LENGTH = 20;
+
   const [user, setUser] = useState(initialValues);
   const { login_email, login_password } = user;
 
@@ -21,9 +26,6 @@ const SignIn = () => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
-
-  const MIN_PASSWORD_LENGTH = 6;
-  const MAX_PASSWORD_LENGTH = 20;
 
   // Validation schema with Yup
   const signInValidation = Yup.object().shape({
@@ -36,6 +38,7 @@ const SignIn = () => {
       .max(20, `Password is too long (maximum ${MAX_PASSWORD_LENGTH} characters)`),
   });
 
+  console.log(providers);
   return (
     <div className={styles.signin}>
       <div className={styles.signin__container}>
@@ -80,6 +83,16 @@ const SignIn = () => {
               </Form>
             )}
           </Formik>
+          <div className={styles.signin__providers}>
+            <span>Or sign in with</span>
+            {Object.values(providers).map((provider) => (
+              <div key={provider.id} className={styles.provider_container}>
+                <Button type="google" onClick={() => signIn(provider.id)}>
+                  <FcGoogle /> Google sign in
+                </Button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
@@ -87,3 +100,18 @@ const SignIn = () => {
 };
 
 export default SignIn;
+
+export async function getServerSideProps() {
+  try {
+    const providers = await getProviders();
+
+    return {
+      props: { providers },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: { providers: [] }, // return empty providers array if there's an error
+    };
+  }
+}
