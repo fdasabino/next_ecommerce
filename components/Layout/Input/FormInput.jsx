1709d1048dccdfa145ef0154ffdc77910495eba0
Francisco Sabino <fdasabino@gmail.com>
2023-06-13 17:26:34 +0200

    added validation for signin form with yup

--- components/Layout/Input/FormInput.jsx
+++ components/Layout/Input/FormInput.jsx
@@ -1,20 +1,41 @@
+import { ErrorMessage, useField } from "formik";
 import { BiUser } from "react-icons/bi";
 import { IoKeyOutline } from "react-icons/io5";
-import { SiMinutemailer } from "react-icons/si";
+import { MdAlternateEmail, MdErrorOutline } from "react-icons/md";
 import styles from "./FormInput.module.scss";
 
-const FormInput = ({ icon, placeholder }) => {
+const iconMap = {
+  user: <BiUser />,
+  email: <MdAlternateEmail />,
+  password: <IoKeyOutline />,
+};
+
+const FormInput = ({ icon, placeholder, ...props }) => {
+  const [field, meta] = useField(props);
+
   return (
-    <div className={styles.form_input}>
-      {icon === "user" ? (
-        <BiUser />
-      ) : icon === "email" ? (
-        <SiMinutemailer />
-      ) : icon === "password" ? (
-        <IoKeyOutline />
-      ) : null}
-      <input type="text" placeholder={placeholder} />
-    </div>
+    <>
+      {meta.error && meta.touched && (
+        <div className={styles.error_message}>
+          <span></span>
+          <p>
+            <ErrorMessage name={field.name} />
+          </p>
+          <MdErrorOutline />
+        </div>
+      )}
+      <div
+        className={`${styles.form_input} ${meta.touched && meta.error ? styles.wrapper_error : ""}`}
+      >
+        {icon && iconMap[icon]}
+        <input
+          className={`${meta.touched && meta.error ? styles.input_error : ""}`}
+          {...field}
+          {...props}
+          placeholder={placeholder}
+        />
+      </div>
+    </>
   );
 };
 
--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -1,10 +1,41 @@
+import Button from "@/components/Layout/Button/Button";
 import FormInput from "@/components/Layout/Input/FormInput";
 import styles from "@/styles/SignIn.module.scss";
 import { Form, Formik } from "formik";
 import Link from "next/link";
+import { useState } from "react";
+import { AiOutlineArrowRight } from "react-icons/ai";
 import { BiLeftArrowAlt } from "react-icons/bi";
+import * as Yup from "yup";
+
+const initialValues = {
+  login_email: "",
+  login_password: "",
+};
 
 const SignIn = () => {
+  const [user, setUser] = useState(initialValues);
+  const { login_email, login_password } = user;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const MIN_PASSWORD_LENGTH = 6;
+  const MAX_PASSWORD_LENGTH = 20;
+
+  // Validation schema with Yup
+  const signInValidation = Yup.object().shape({
+    login_email: Yup.string()
+      .email("Please enter a valid email address...")
+      .required("Email address is required..."),
+    login_password: Yup.string()
+      .required("Password is required...")
+      .min(6, `Password is too short (minimun ${MIN_PASSWORD_LENGTH} characters)`)
+      .max(20, `Password is too long (maximum ${MAX_PASSWORD_LENGTH} characters)`),
+  });
+
   return (
     <div className={styles.signin}>
       <div className={styles.signin__container}>
@@ -19,10 +50,30 @@ const SignIn = () => {
         <div className={styles.signin__form}>
           <h1>Sign in</h1>
           <p>Sign in with your account</p>
-          <Formik>
+          <Formik
+            enableReinitialize
+            initialValues={{ login_email, login_password }}
+            validationSchema={signInValidation}
+          >
             {(form) => (
               <Form>
-                <FormInput icon="password" placeholder="Enter your password" />
+                <FormInput
+                  type="email"
+                  icon="email"
+                  name="login_email"
+                  placeholder="Email address"
+                  onChange={handleChange}
+                />
+                <FormInput
+                  type="password"
+                  icon="password"
+                  name="login_password"
+                  placeholder="Password"
+                  onChange={handleChange}
+                />
+                <Button>
+                  Sign in <AiOutlineArrowRight />
+                </Button>
               </Form>
             )}
           </Formik>
