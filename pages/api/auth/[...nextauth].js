6d13c88402687fcbb51ec37ad2a046f255bc7b38
Francisco Sabino <fdasabino@gmail.com>
2023-06-15 22:18:40 +0200

    added sign in logic backend

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,13 @@
+import User from "@/models/User";
+import db from "@/utils/db";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogProvider from "next-auth/providers/google";
 import clientPromise from "./lib/mongodb";
+
+db.connectDB();
 
 export const authOptions = {
   // adapter
@@ -13,7 +19,25 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials.email;
+        const password = credentials.password;
+
+        // find user in db
+        const user = await User.findOne({ email });
+        if (user) {
+          return signInUser(password, user);
+        } else {
+          throw new Error("Invalid credentials...");
+        }
+      },
+    }),
   ],
   pages: {
     signIn: "/auth/signin",
@@ -25,3 +49,13 @@ export const authOptions = {
 };
 
 export default NextAuth(authOptions);
+
+async function signInUser(password, user) {
+  if (!user.password) {
+    throw new Error("Please enter a password");
+  }
+
+  const enteredPassword = await bcrypt.compare(password, user.password);
+  if (!enteredPassword) throw new Error("Invalid credentials...Please try again");
+  return user;
+}
