121298b587781c512344474941fa2ce7ffa95a15
Francisco Sabino <fdasabino@gmail.com>
2023-06-17 00:10:03 +0200

    added container class on base.scss

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,7 +18,9 @@ const Home = ({ country }) => {
 
   return (
     <div className={styles.home}>
-      <h1>Home Page</h1>
+      <div className={styles.container}>
+        <h1>Home</h1>
+      </div>
     </div>
   );
 };
