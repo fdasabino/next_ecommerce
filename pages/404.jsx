098363d1c9b846561ea0caad9cc981ea73018a87
Francisco Sabino <fdasabino@gmail.com>
2023-06-17 21:33:52 +0200

    added personalized 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,25 @@
+import styles from "@/styles/pages/NotFound.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import NotFoundImg from "/public/images/404.png";
+
+const NotFound = () => {
+  return (
+    <div className={styles.not_found}>
+      <div className={styles.not_found__container}>
+        <Image src={NotFoundImg} width={500} height={500} alt="notfound" />
+        <h1>OOPs!</h1>
+        <h2>Page Not Found</h2>
+        <p>
+          It seems like the page you are looking for does not exist! <br />
+          <Link href="/">
+            <AiOutlineArrowLeft /> Please take me back!{" "}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
