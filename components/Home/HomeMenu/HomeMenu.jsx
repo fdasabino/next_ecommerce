568d96eec050fbf984e904eeee2a6ab31b3189c0
Francisco Sabino <fdasabino@gmail.com>
2023-06-17 18:14:21 +0200

    added placeholder content home menu

--- components/Home/HomeMenu/HomeMenu.jsx
+++ components/Home/HomeMenu/HomeMenu.jsx
@@ -1,8 +1,30 @@
 import { menuArray } from "@/data/home_data";
+import Link from "next/link";
+import { BiCategory } from "react-icons/bi";
 import styles from "./HomeMenu.module.scss";
 
 const HomeMenu = () => {
-  return <div className={styles.home_menu}>menu</div>;
+  return (
+    <div className={styles.home_menu}>
+      <ul>
+        <li>
+          <a className={styles.home_menu__header}>
+            <BiCategory />
+            <b>Categories</b>
+          </a>
+        </li>
+        <div className={styles.home_menu__list}>
+          {menuArray.map((item, i) => (
+            <li key={i}>
+              <Link href={item.link}>
+                <span>{item.name}</span>
+              </Link>
+            </li>
+          ))}
+        </div>
+      </ul>
+    </div>
+  );
 };
 
 export default HomeMenu;
