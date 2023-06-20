60c2d44cfdede9d2bd5cd25df5caf2bc0ddf486c
Francisco Sabino <fdasabino@gmail.com>
2023-06-20 14:57:58 +0200

    added google fonts links

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link href="https://fonts.googleapis.com/css?family=Mulish:200,300,regular,500,600,700,800,900,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic&display=swap" rel="stylesheet"  />
+      <link href="https://fonts.googleapis.com/css?family=Rajdhani:300,regular,500,600,700&display=swap" rel="stylesheet" />
       <body>
         <Main />
         <NextScript />
