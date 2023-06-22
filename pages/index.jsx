11c00dfd816fcf9651fd5f343781c5fe6cb015af
Francisco Sabino <fdasabino@gmail.com>
2023-06-22 20:28:17 +0200

    update pages/index.jsx and redux-store/index.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,32 @@
 import FlashDeals from "@/components/Home/FlashDeals/FlashDeals";
 import MainSection from "@/components/Home/MainSection/MainSection";
 import { setCountry } from "@/redux-store/countrySlice";
+import { setProducts } from "@/redux-store/productSlice";
 import styles from "@/styles/pages/Home.module.scss";
+import db from "@/utils/db";
 import axios from "axios";
 import { useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import Products from "../models/Products";
 
-const Home = ({ country }) => {
+const Home = ({ country, products }) => {
   const dispatch = useDispatch();
+
   // Set country in redux store
   const setCountryToStore = useCallback(() => {
     dispatch(setCountry(country));
   }, [country, dispatch]);
 
+  // Set products in redux store
+  const setProductsToStore = useCallback(() => {
+    dispatch(setProducts(products));
+  }, [dispatch, products]);
+
   // Set country in redux store on page load
   useEffect(() => {
+    setProductsToStore();
     setCountryToStore();
-  }, [setCountryToStore]);
+  }, [setCountryToStore, setProductsToStore]);
 
   return (
     <div className={styles.home}>
@@ -31,7 +41,11 @@ const Home = ({ country }) => {
 export default Home;
 
 export async function getServerSideProps() {
+  db.connectDB();
+
   try {
+    let products = await Products.find({}).sort({ createdAt: -1 }).lean();
+
     const { data } = await axios.get(
       `https://api.ipregistry.co/?key=${process.env.NEXT_APP_IP_REGISTRY_API}`
     );
@@ -39,6 +53,7 @@ export async function getServerSideProps() {
 
     return {
       props: {
+        products: JSON.parse(JSON.stringify(products)),
         country,
       },
     };
