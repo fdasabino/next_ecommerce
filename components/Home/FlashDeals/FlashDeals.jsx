1ebaa73912aa6aea5f097ef09a677201a96eb17d
Francisco Sabino <fdasabino@gmail.com>
2023-06-23 00:09:52 +0200

    update Card.module.scss, FlashDeals.jsx, FlashDealsSwiper.module.scss and ProductSwiper.module.scss

--- components/Home/FlashDeals/FlashDeals.jsx
+++ components/Home/FlashDeals/FlashDeals.jsx
@@ -1,9 +1,11 @@
 import CountDown from "@/components/Layout/CountDown/CountDown";
 import { MdFlashOn } from "react-icons/md";
+import { useMediaQuery } from "react-responsive";
 import FlashDealsSwiper from "../FlashDealsSwiper/FlashDealsSwiper";
 import styles from "./FlashDeals.module.scss";
 
 const FlashDeals = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   return (
     <section className={styles.flash_deals}>
       <div className={styles.flash_deals__header}>
@@ -12,9 +14,11 @@ const FlashDeals = () => {
           <MdFlashOn />
         </h2>
       </div>
-      <div className={styles.flash_deals__countdown}>
-        <CountDown date={new Date(2023, 6, 23)} />
-      </div>
+      {!isMobile && (
+        <div className={styles.flash_deals__countdown}>
+          <CountDown date={new Date(2023, 6, 23)} />
+        </div>
+      )}
       <div className={styles.flash_deals__container}>
         <FlashDealsSwiper />
       </div>
