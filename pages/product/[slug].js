118bf5a38513903bdbbab735e0aeac5c7d8a1416
Francisco Sabino <fdasabino@gmail.com>
2023-06-23 12:20:23 +0200

    added single product page and server side

---
+++ pages/product/[slug].js
@@ -0,0 +1,54 @@
+import Product from "@/models/Products";
+import styles from "@/styles/pages/SingleProductPage.module.scss";
+import db from "@/utils/db";
+
+const SingleProductPage = ({ product }) => {
+  console.log(product);
+  return <div className={styles.single_product_page}>SingleProductPage</div>;
+};
+
+export default SingleProductPage;
+
+export async function getServerSideProps(context) {
+  db.connectDB();
+  try {
+    const { query } = context;
+    const slug = query.slug;
+    const color = query.color;
+    const size = query.size || 0;
+
+    const product = await Product.findOne({ slug }).lean().exec();
+    const subProduct = product.subProducts[color];
+    const prices = subProduct.sizes.map((size) => size.price).sort((a, b) => a - b);
+
+    const newProduct = {
+      ...product,
+      images: subProduct.images,
+      sizes: subProduct.sizes,
+      discount: subProduct.discount,
+      sku: subProduct.sku,
+      colors: product.subProducts.map((subProduct) => subProduct.color),
+      priceRange: prices.length > 1 ? `From: ${prices[0]}$ to: ${prices[prices.length - 1]}$` : "",
+      priceBeforeDiscount: subProduct.sizes[size].price.toFixed(2),
+      price:
+        subProduct.discount > 0
+          ? (
+              subProduct.sizes[size].price -
+              subProduct.sizes[size].price / subProduct.discount
+            ).toFixed(2)
+          : (subProduct.sizes[size].price * 1).toFixed(2),
+
+      quantity: subProduct.sizes[size].qty,
+    };
+    return {
+      props: { product: JSON.parse(JSON.stringify(newProduct)) }, // will be passed to the page component as props
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: { product: null },
+    };
+  } finally {
+    db.disconnectDB();
+  }
+}
