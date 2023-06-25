2a51abe18e465821c4a62cb6eaf5390767009c0c
Francisco Sabino <fdasabino@gmail.com>
2023-06-25 19:29:08 +0200

    update ProductInfo.jsx and ProductInfo.module.scss

--- components/ProductPage/ProductInfo/ProductInfo.jsx
+++ components/ProductPage/ProductInfo/ProductInfo.jsx
@@ -1,7 +1,20 @@
 import { Rating } from "@mui/material";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import styles from "./ProductInfo.module.scss";
 
 const ProductInfo = ({ product, setActiveImage }) => {
+  const router = useRouter();
+  const { color, size } = router.query;
+  const [selectedSize, setSelectedSize] = useState(+size);
+
+  useEffect(() => {
+    if (!selectedSize) {
+      setSelectedSize(size);
+    }
+  }, [selectedSize, size]);
+
   return (
     <div className={styles.product_info}>
       <div className={styles.product_info__container}>
@@ -18,20 +31,69 @@ const ProductInfo = ({ product, setActiveImage }) => {
           </p>
         </div>
 
+        {/* discount */}
+        {selectedSize && (
+          <div className={styles.product_info__discount}>
+            {product.discount > 0 && (
+              <>
+                <small>{product.priceBeforeDiscount}$</small>
+                <p>(-{product.discount}%)</p>
+              </>
+            )}
+          </div>
+        )}
+
         {/* price */}
         <div className={styles.product_info__price}>
-          <p>{product.priceRange ? product.priceRange : Math.floor(product.price).toFixed(2)}</p>
+          {!selectedSize && product.priceRange ? (
+            <p>{product.priceRange}</p>
+          ) : (
+            <h2>{Math.floor(product.price).toFixed(2)}$</h2>
+          )}
         </div>
 
-        {/* discount */}
-        <div className={styles.product_info__discount}>
-          {product.discount > 0 && (
-            <>
-              <small>{product.priceBeforeDiscount}$</small>
-              <p>(-{product.discount}%)</p>
-              <h2>{product.price}$</h2>
-            </>
+        {/* Shipping */}
+        <div className={styles.product_info__shipping}>
+          {product.shipping ? (
+            <small> {`+${product.shipping}$ shipping fee`}</small>
+          ) : (
+            <span>Free Shipping</span>
           )}
+        </div>
+
+        {/* quantity available */}
+        <div className={styles.product_info__quantity}>
+          {selectedSize ? (
+            <span>Availability: {product.quantity} items available</span>
+          ) : (
+            <small>{`Availability: ${product.sizes.reduce(
+              (acc, curr) => acc + curr.qty,
+              0
+            )} items for all sizes`}</small>
+          )}
+        </div>
+
+        {/* sizes */}
+        <div className={styles.product_info__sizes}>
+          {!selectedSize && <h4>Select a size:</h4>}
+          <div className={styles.wrapper}>
+            {product.sizes.map((s, i) => (
+              <Link
+                onClick={() => setSelectedSize(i)}
+                key={i}
+                href={`/product/${product.slug}?color=${color}&size=${i}`}
+              >
+                {console.log(+size, i)}
+                <div
+                  className={`${styles.product_info__sizes_size} ${
+                    +size === i && styles.activeSize
+                  }`}
+                >
+                  {s.size}
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
