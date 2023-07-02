d982d651456cac54334b198c10f68cdb311f2073
Francisco Sabino <fdasabino@gmail.com>
2023-07-02 09:24:14 +0200

    update pages/cart/index.jsx

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -1,7 +1,7 @@
 import styles from "@/styles/pages/CartPage.module.scss";
 
 const CartPage = () => {
-  return <div className={styles.cart}>CartPage</div>;
+  return <div className={styles.cart}>cart</div>;
 };
 
 export default CartPage;
