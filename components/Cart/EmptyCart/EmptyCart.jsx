50c4b9fabef37364baf9ff8e1c60908f577f3fbd
Francisco Sabino <fdasabino@gmail.com>
2023-07-03 11:42:55 +0200

    update EmptyCart.jsx, EmptyCart.module.scss, pages/404/index.jsx and NotFound.module.scss

--- components/Cart/EmptyCart/EmptyCart.jsx
+++ components/Cart/EmptyCart/EmptyCart.jsx
@@ -1,16 +1,35 @@
+import Button from "@/components/Layout/Button/Button";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import styles from "./EmptyCart.module.scss";
 
 const EmptyCart = () => {
+  const router = useRouter();
+
+  const { data: session } = useSession();
+  const handleSignIn = () => {
+    signIn();
+  };
+
+  const handleRedirect = () => {
+    router.push("/");
+  };
+
   return (
     <div className={styles.empty__cart}>
       <Image src={"/images/empty_cart.png"} width={500} height={500} alt="empty cart" />
       <p>Your cart is empty, but it doesn&#8217;t have to be... </p>
-      <Link href="/browse">
-        <AiOutlineArrowLeft /> Browse our shop
-      </Link>
+      {session ? (
+        <Button onClick={handleRedirect} style="primary">
+          <AiOutlineArrowLeft /> Take me shopping
+        </Button>
+      ) : (
+        <Button onClick={handleSignIn} style="primary">
+          Sign in/Sign up
+        </Button>
+      )}
     </div>
   );
 };
--- pages/404/index.jsx
+++ pages/404/index.jsx
@@ -1,10 +1,17 @@
+import Button from "@/components/Layout/Button/Button";
 import styles from "@/styles/pages/NotFound.module.scss";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import NotFoundImg from "/public/images/404.png";
 
 const NotFound = () => {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push("/");
+  };
+
   return (
     <div className={styles.not_found}>
       <div className={styles.not_found__container}>
@@ -15,10 +22,10 @@ const NotFound = () => {
           It seems like the page you are looking for does not exist!
           <br />
           <br />
-          <Link href="/">
-            <AiOutlineArrowLeft /> Please take me back!{" "}
-          </Link>
         </p>
+        <Button onClick={handleRedirect} style="primary">
+          <AiOutlineArrowLeft /> Take me back
+        </Button>
       </div>
     </div>
   );
