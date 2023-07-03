2aafd0d1ff8cf054555b12293ba6dd345a4513b4
Francisco Sabino <fdasabino@gmail.com>
2023-07-03 13:30:34 +0200

    update cartSlice.js

--- redux-store/cartSlice.js
+++ redux-store/cartSlice.js
@@ -1,9 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  cartItems: [],
+};
+
 export const cartSlice = createSlice({
   name: "cart",
-  initialState: [],
-  reducers: {},
+  initialState: initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      state.cartItems.push(action.payload);
+    },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((item) => item.id !== action.payload.id);
+    },
+    updateCart: (state, action) => {
+      state.cartItems = action.payload;
+    },
+    clearCart: (state, action) => {
+      state.cartItems = [];
+    },
+  },
 });
+
+export const { addToCart, removeFromCart, updateCart, clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
