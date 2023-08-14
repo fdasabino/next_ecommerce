7ebae8a67ddc7a1ed708c22bfe3009db91771e01
Francisco Sabino <fdasabino@gmail.com>
2023-08-14 15:06:34 +0200

    update addCartTodb.js

--- utils/addCartTodb.js
+++ utils/addCartTodb.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export const addCartToDb = async (cart, userID) => {
   try {
     const { data } = await axios.post("/api/user/saveCartToDb", { cart, userID });
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error.message);
