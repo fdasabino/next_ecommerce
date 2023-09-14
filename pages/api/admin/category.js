f0f1afba26c45fe3cb66b2f9af89659bd9b0ab27
Francisco Sabino <fdasabino@gmail.com>
2023-09-15 00:41:35 +0200

    update category.js - added auth middleware

--- pages/api/admin/category.js
+++ pages/api/admin/category.js
@@ -1,3 +1,4 @@
+import authMiddleware from "@/middleware/auth";
 import Category from "@/models/Category";
 import db from "@/utils/db";
 import slugify from "slugify";
@@ -8,57 +9,59 @@ const handler = async (req, res) => {
   const { name } = req.body;
 
   try {
-    // create category
-    if (method === "POST") {
-      const categoryExists = await Category.findOne({ name });
+    await authMiddleware(req, res, async () => {
+      // create category
+      if (method === "POST") {
+        const categoryExists = await Category.findOne({ name });
 
-      if (categoryExists) {
-        return res.status(400).json({ error: "Category already exists", ok: false });
+        if (categoryExists) {
+          return res.status(400).json({ error: "Category already exists", ok: false });
+        }
+
+        await new Category({ name, slug: slugify(name) }).save();
+
+        return res.status(201).json({
+          message: `Category "${name}" has been successfully created...`,
+          ok: true,
+          categories: await Category.find({}).sort({ createdAt: -1 }),
+        });
       }
 
-      await new Category({ name, slug: slugify(name) }).save();
+      // update category
+      if (method === "PATCH") {
+        const { id, name } = req.body.data;
+        const category = await Category.findByIdAndUpdate(id);
 
-      return res.status(201).json({
-        message: `Category "${name}" has been successfully created...`,
-        ok: true,
-        categories: await Category.find({}).sort({ createdAt: -1 }),
-      });
-    }
+        if (category.name === name) {
+          return res.status(400).json({ error: "Category already exists", ok: false });
+        }
 
-    // update category
-    if (method === "PATCH") {
-      const { id, name } = req.body.data;
-      const category = await Category.findByIdAndUpdate(id);
+        category.name = name;
+        category.slug = slugify(name);
+        await category.save();
 
-      if (category.name === name) {
-        return res.status(400).json({ error: "Category already exists", ok: false });
+        return res.status(200).json({
+          message: `Category "${name}" has been successfully updated...`,
+          ok: true,
+          categories: await Category.find({}).sort({ createdAt: -1 }),
+        });
       }
 
-      category.name = name;
-      category.slug = slugify(name);
-      await category.save();
-
-      return res.status(200).json({
-        message: `Category "${name}" has been successfully updated...`,
-        ok: true,
-        categories: await Category.find({}).sort({ createdAt: -1 }),
-      });
-    }
-
-    // delete category
-    if (method === "PUT") {
-      const { id } = req.body.data;
-      const category = await Category.findByIdAndDelete(id);
-      if (!category) {
-        return res.status(400).json({ error: "Category not found", ok: false });
+      // delete category
+      if (method === "PUT") {
+        const { id } = req.body.data;
+        const category = await Category.findByIdAndDelete(id);
+        if (!category) {
+          return res.status(400).json({ error: "Category not found", ok: false });
+        }
+        const categories = await Category.find({}).sort({ createdAt: -1 });
+        return res.status(200).json({
+          message: "Category has been successfully deleted...",
+          ok: true,
+          categories,
+        });
       }
-      const categories = await Category.find({}).sort({ createdAt: -1 });
-      return res.status(200).json({
-        message: "Category has been successfully deleted...",
-        ok: true,
-        categories,
-      });
-    }
+    });
   } catch (error) {
     return res.status(500).json({ error: error.message, ok: false });
   } finally {
