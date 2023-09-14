ec14f36856497decb60b3d0497ec14946f36704d
Francisco Sabino <fdasabino@gmail.com>
2023-09-15 00:38:48 +0200

    update auth.js - to avoid stalled reqs

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,18 @@ const secret = process.env.JWT_SECRET;
 const secureCookie = process.env.NODE_ENV === "production";
 
 const authMiddleware = async (req, res, next) => {
-  const token = await getToken({ req, secret, secureCookie });
-  if (token) {
-    req.user = token.sub;
-    console.log("User authenticated can access protected routes");
-    next();
-  } else {
-    res.status(401).json({ error: "Unauthorized" });
+  try {
+    const token = await getToken({ req, secret, secureCookie });
+
+    if (token) {
+      req.user = token.sub;
+      console.log("User authenticated can access protected routes");
+    }
+  } catch (error) {
+    return res.status(401).json({ error: "Unauthorized" });
   }
+
+  await next();
 };
 
 export default authMiddleware;
