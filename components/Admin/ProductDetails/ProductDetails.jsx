bacf4ba7639c92c9861d53550356cb0078fc9ba1
Francisco Sabino <fdasabino@gmail.com>
2023-09-16 21:58:56 +0200

    update ProductDetails.jsx and ProductDetails.module.scss

--- components/Admin/ProductDetails/ProductDetails.jsx
+++ components/Admin/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,13 @@
 import { GetColorName } from "hex-color-to-color-name";
 import Image from "next/image";
+import { Navigation } from "swiper";
+import SwiperCore, { Pagination } from "swiper/core";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css"; // Import Swiper styles
 import styles from "./ProductDetails.module.scss";
+
+// Initialize SwiperCore with required modules
+SwiperCore.use([Pagination, Navigation]);
 
 const ProductDetails = ({ product }) => {
   const getColorName = (color) => {
@@ -11,45 +18,71 @@ const ProductDetails = ({ product }) => {
   return (
     <div className={styles.product_details}>
       <div className={styles.sub_products}>
-        {product.subProducts.map((sub, i) => {
-          const { images, sizes } = sub;
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={5}
+          loop={true}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            500: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            600: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            800: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+          }}
+        >
+          {product.subProducts.map((sub, i) => {
+            const { images, sizes } = sub;
 
-          return (
-            <div key={i} className={styles.sub_product}>
-              <div className={styles.sub_images}>
-                {images.map((image, i) => (
-                  <Image
-                    key={i}
-                    src={image.url}
-                    width={150}
-                    height={150}
-                    alt={`${getColorName(sub.color.color)}, ${product.name}`}
-                  />
-                ))}
-              </div>
-              <div className={styles.sub_info}>
-                <small>
-                  Color:
-                  <span>{getColorName(sub.color.color)}</span>
-                </small>
-                <small>
-                  Discount:
-                  <span>{sub.discount}%</span>
-                </small>
-              </div>
-              <div className={styles.sub_sizes}>
-                <small>
-                  Sizes:
-                  <div className={styles.sizes_wrapper}>
-                    {sizes.map((size, i) => (
-                      <span key={i}>{size.size}</span>
+            return (
+              <SwiperSlide key={i}>
+                <div className={styles.sub_product}>
+                  <div className={styles.sub_images}>
+                    {images.map((image, j) => (
+                      <Image
+                        key={j}
+                        src={image.url}
+                        width={150}
+                        height={150}
+                        alt={`${getColorName(sub.color.color)}, ${product.name}`}
+                      />
                     ))}
                   </div>
-                </small>
-              </div>
-            </div>
-          );
-        })}
+                  <div className={styles.sub_info}>
+                    <small>
+                      Color:
+                      <span>{getColorName(sub.color.color)}</span>
+                    </small>
+                    <small>
+                      Discount:
+                      <span>{sub.discount}%</span>
+                    </small>
+                  </div>
+                  <div className={styles.sub_sizes}>
+                    <small>
+                      Sizes:
+                      <div className={styles.sizes_wrapper}>
+                        {sizes.map((size, k) => (
+                          <span key={k}>{size.size}</span>
+                        ))}
+                      </div>
+                    </small>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   );
