9abcda06a469aaa87ff56230788ef8cb5f25e293
Francisco Sabino <fdasabino@gmail.com>
2023-09-20 21:36:52 +0200

    changes in db connection

--- utils/db.js
+++ utils/db.js
@@ -1,46 +1,36 @@
 import mongoose from "mongoose";
+const connection = {};
 
-let connection;
-
-// * connect to database
-const connectDB = async () => {
-  if (connection && mongoose.connection.readyState === 1) {
-    console.log("Already connected to the database...");
+async function connectDB() {
+  if (connection.isConnected) {
+    console.log("Already connected to the database.");
     return;
   }
-
-  try {
-    connection = await mongoose.connect(process.env.NEXT_APP_MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("New database connection...");
-  } catch (error) {
-    console.error("Database connection error:", error.message);
-    throw new Error(error.message);
-  }
-};
-
-connectDB();
-
-// * disconnect from database (only for production)
-const disconnectDB = async () => {
-  if (connection && mongoose.connection.readyState === 1) {
-    if (process.env.NODE_ENV !== "production") {
-      console.log("Not disconnected from the database...");
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log("Use previous connection to the database.");
       return;
     }
+    await mongoose.disconnect();
+  }
+  const db = await mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("New connection to the database.");
+  connection.isConnected = db.connections[0].readyState;
+}
 
-    try {
+async function disconnectDB() {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === "production") {
       await mongoose.disconnect();
       connection.isConnected = false;
-      console.log("Disconnected from the database...");
-    } catch (error) {
-      console.error("Database disconnection error:", error.message);
-      throw new Error(error.message);
+    } else {
+      console.log("Not disconnecting from the database.");
     }
   }
-};
-
+}
 const db = { connectDB, disconnectDB };
 export default db;
