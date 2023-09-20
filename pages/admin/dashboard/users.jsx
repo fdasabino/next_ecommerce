bf7e12e7035b044223c8b769608d4c308fc5fa5e
Francisco Sabino <fdasabino@gmail.com>
2023-09-20 21:09:54 +0200

    added users to user dashboard with MUI table component

--- pages/admin/dashboard/users.jsx
+++ pages/admin/dashboard/users.jsx
@@ -1,18 +1,22 @@
 import AdminLayout from "@/components/Admin/AdminLayout/AdminLayout";
+import UsersList from "@/components/Admin/UsersList/UsersList";
 import User from "@/models/User";
+import styles from "@/styles/pages/Users.module.scss";
 import db from "@/utils/db";
 import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
 
-const AdminUsers = ({ user }) => {
+const AdminUsers = ({ user, users }) => {
   const router = useRouter();
   const { pathname } = router;
   const path = pathname.split("/admin/dashboard")[1];
 
   return (
     <AdminLayout path={path} user={user}>
-      AdminUsers
+      <div className={styles.users}>
+        <UsersList rows={users} />
+      </div>
     </AdminLayout>
   );
 };
@@ -25,9 +29,12 @@ export async function getServerSideProps(context) {
   const session = await getSession(context);
   try {
     const user = await User.findOne({ _id: session.user._id }).lean();
+    const users = await User.find({}).sort({ createdAt: -1 }).lean();
+
     return {
       props: {
         user: JSON.parse(JSON.stringify(user)),
+        users: JSON.parse(JSON.stringify(users)),
       },
     };
   } catch (error) {
