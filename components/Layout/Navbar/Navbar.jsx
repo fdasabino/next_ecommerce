8487dc002b3c3f072d5d3a3a2e24a0a844626c47
Francisco Sabino <fdasabino@gmail.com>
2023-10-05 21:19:02 +0200

    update AdminLayout.module.scss, Navbar.jsx and Notifications.module.scss

--- components/Layout/Navbar/Navbar.jsx
+++ components/Layout/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
+import { useRouter } from "next/router";
 import Main from "./Main/Main";
 import Menu from "./Menu/Menu";
 import styles from "./Navbar.module.scss";
 import Top from "./Top/Top";
 
 const Navbar = () => {
+  const router = useRouter();
   return (
     <header className={styles.navbar}>
       <Top />
       <Menu />
-      <Main />
+      {!router.pathname.startsWith("/admin") && <Main />}
     </header>
   );
 };
