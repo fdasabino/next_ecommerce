f612c99cdfa3936b2b79cca138be6cf51614c228
Francisco Sabino <fdasabino@gmail.com>
2023-10-28 21:58:14 +0200

    chore: delete loading.js

--- pages/loading.js
+++
@@ -1,6 +0,0 @@
-import Loader from "@/components/Layout/Loader/Loader";
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <Loader />;
-}
