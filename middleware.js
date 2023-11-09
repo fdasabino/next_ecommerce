87e1bc3d0afcae6206ddd70799f53deca7bf9030
Francisco Sabino <fdasabino@gmail.com>
2023-11-09 16:08:21 +0100

    Add session check for /profile route in middleware

--- middleware.js
+++ middleware.js
@@ -19,6 +19,10 @@ const middleware = async (req) => {
         if (!session) return NextResponse.redirect(`${origin}`);
     }
 
+    if (pathname.startsWith("/profile")) {
+        if (!session) return NextResponse.redirect(`${origin}`);
+    }
+
     if (pathname.startsWith("/admin")) {
         if (!session) return NextResponse.redirect(`${origin}`);
         if (session.role !== "admin") return NextResponse.redirect(`${origin}`);
