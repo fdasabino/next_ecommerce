5a6dab0b5ee36e2a7d2006442d2b1a8fd8392626
Francisco Sabino <fdasabino@gmail.com>
2024-01-09 17:14:57 +0100

    Add order details and link to order page

--- components/Profile/OrderCard/OrderCard.jsx
+++ components/Profile/OrderCard/OrderCard.jsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import React from "react";
+import { IoIosArrowDropright } from "react-icons/io";
 import styles from "./OrderCard.module.scss";
 const OrderCard = ({ order }) => {
     console.log(order);
@@ -11,13 +13,28 @@ const OrderCard = ({ order }) => {
 
     const formattedId = order._id.substring(10, 24).toUpperCase();
 
+    const formattedTotal = order.total.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+    });
+
     return (
         <div className={styles.order__card}>
-            <div className={styles.order__card__header}>
+            <div className={styles.order__card__top}>
                 <h2>{formattedDate}</h2>
                 <p>{formattedId}</p>
             </div>
-            <div className={styles.order__card__body}></div>
+            <div className={styles.order__card__bottom}>
+                <h3>Total: {formattedTotal}</h3>
+                <h3>Status: {order.status}</h3>
+                <h3>Payment: {order.paymentMethod}</h3>
+                <div className={styles.order__card__bottom__details}>
+                    <Link href={`/order/${order._id}`}>
+                        View Details
+                        <IoIosArrowDropright />
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
